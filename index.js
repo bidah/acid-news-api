4b57f38d430d647faa807b5091de7b0bdaa25865
bidah <bartoindahouse@gmail.com>
2018-07-11 19:59:52 -0400

    wip get data

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(3001, () => console.log("Server ready"));
 
 // var mongo = require("./mongo");
+//
+const apiUrl = 'http://hn.algolia.com/api/v1/search_by_date?query=nodejs'
+
+let getNewsFeed  = async () => {
+  setInterval(()=> {
+    let jsonRes = await fetch(apiUrl).then(res => res.json())
+
+  })
+} 
 
 app.get("*/health", (req, res) => res.sendStatus(200));
 
