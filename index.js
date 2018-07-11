8813a3ede368bf39660cc8154fe35c39c459c661
bidah <bartoindahouse@gmail.com>
2018-07-11 19:26:14 -0400

    health route test

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = require("express")();
 const fetch = require("node-fetch");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(process.env.NODEMON ? 8080 : 80, () => console.log("Server ready"));
+app.listen(3001, () => console.log("Server ready"));
 
-var mongo = require("./mongo");
+// var mongo = require("./mongo");
+
+app.get("*/health", (req, res) => res.sendStatus(200));
 
