500d33d96c9846633b5d24735bfecd7a6222240c
bidah <bartoindahouse@gmail.com>
2018-07-11 20:08:40 -0400

    listening on port 3000

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = require("express")();
 const fetch = require("node-fetch");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(3001, () => console.log("Server ready"));
+app.listen(3000, () => console.log("Server ready"));
 
 // var mongo = require("./mongo");
 //
