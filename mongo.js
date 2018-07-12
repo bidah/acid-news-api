4eff5bd1548b667187c3b82cd2bf54db15c5c113
bidah <bartoindahouse@gmail.com>
2018-07-11 20:44:24 -0400

    changez

--- mongo.js
+++ mongo.js
@@ -1,6 +1,8 @@
 const mongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectID;
 
+const DB;
+
 mongoClient.connect(
   process.env.MONGO_HOST,
   (e, db) => (e ? console.log(e) : (DB = db))
