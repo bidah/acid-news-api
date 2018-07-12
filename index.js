bb2a80b028cae74ab6d0a6faaa9d3522f1a9d809
bidah <bartoindahouse@gmail.com>
2018-07-12 09:17:37 -0400

    server health ok

--- index.js
+++ index.js
@@ -5,16 +5,31 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(3000, () => console.log("Server ready"));
 
+const mongo = require("./mongo");
+
 // var mongo = require("./mongo");
 //
 const apiUrl = 'http://hn.algolia.com/api/v1/search_by_date?query=nodejs'
 
-let getNewsFeed  = async () => {
-  setInterval(()=> {
-    let jsonRes = await fetch(apiUrl).then(res => res.json())
-
-  })
+let getNewsFeed = async () => {
+  // setInterval(()=> {
+  let jsonRes = await fetch(apiUrl).then(res => res.json());
+  // })
+  return jsonRes;
 } 
+
+app.get("*/setData", async (req, res) => {
+
+  let newsFeedJson = await this.getNewsFeed();
+
+  let newsFeedFromDb  = await mongo.setInitial(newsFeedJson)
+
+  if (typeof newsFeedFromDb == "string")
+    return res.json({ status: "error", msg: res });
+
+  // success
+  res.json({ status: "ok", newsFeedFromDb });
+});
 
 app.get("*/health", (req, res) => res.sendStatus(200));
 
--- mongo.js
+++ mongo.js
@@ -1,12 +1,37 @@
 const mongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectID;
 
-const DB;
+var DB;
+const url = "mongodb://localhost:27017";
 
 mongoClient.connect(
-  process.env.MONGO_HOST,
+  url,
   (e, db) => (e ? console.log(e) : (DB = db))
 );
 
+const count = 0;
 module.exports = {
+
+  setInitial(news) {
+    if (count) return;
+
+    return new Promise((resolve, reject) => {
+      DB.collection("newsFeed")
+        .count()
+        .then(num => {
+          if (num) return resolve("already created seed data");
+
+          DB.collection("newsFeed")
+            .insertMany(JSON.parse(news))
+            .then(() => {
+              DB.collection("newsFeed")
+                .find()
+                .toArray((e, num) => {
+                  if (e) return reject(e);
+                  else return resolve(num);
+                });
+            });
+        });
+    });
+  }
 }
