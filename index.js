80475e749c5c0efec453869db66d2a8f9318c6aa
bidah <bartoindahouse@gmail.com>
2018-07-31 12:32:45 -0400

    bug fix

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ if (process.env.REDISTOGO_URL) {
   var rtg   = require("url").parse(process.env.REDISTOGO_URL);
   redisClient = redis.createClient(rtg.port, rtg.hostname);
 
-  redis.auth(rtg.auth.split(":")[1]);
+  redisClient.auth(rtg.auth.split(":")[1]);
 } else {
 
   redisClient = redis.createClient();
